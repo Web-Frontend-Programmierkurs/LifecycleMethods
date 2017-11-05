ec7f3a8d09c3cbbfb75be18c93d8957b8bc8ebe7
Martin Wudenka <Martin.Wudenka@gmx.de>
2017-11-05 18:04:19 +0100

    Implement Counter

--- src/Counter.js
+++ src/Counter.js
@@ -4,6 +4,29 @@ export default class Counter extends Component {
   state = {
     counter: 0
   }
+  interval
+
+  componentDidMount() {
+    this.startCounter()
+  }
+
+  componentWillUnmount() {
+    this.stopCounter()
+  }
+
+  incrementCounter = () => {
+    this.setState(state => ({
+      counter: state.counter + 1
+    }))
+  }
+
+  startCounter = () => {
+    this.interval = setInterval(this.incrementCounter, 1000)
+  }
+
+  stopCounter = () => {
+    clearInterval(this.interval)
+  }
 
   render() {
     return this.state.counter
